5bccfb9331c3c8b0aaad16cb48cd02b93a2ec913
spinshield <146870398+spinshield@users.noreply.github.com>
2024-03-29 21:48:00 +0100

    Update games.php

--- config/games.php
+++ config/games.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "exemptProviders" => array("bgaming", "evoplay"),
+    "exemptProviders" => array("enter_game_provider_you_dont_wannatest", "enter_game_provider_you_dont_wannatest2"),
     "api" => array(
         "endpoint" => env('API_ENDPOINT', ""),
         "login" => env('API_LOGIN', ""),
